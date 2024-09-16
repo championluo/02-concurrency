be05a8f2108b000828fcaef6ec24dd9270d2350c
championLuo <491936385@qq.com>
2024-09-16 19:37:18 +0800

    feature: 并发hashmap 01 代码- 改成了多线程安全的hashmap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -9,11 +9,11 @@ const M: usize = 4;
 fn main() -> Result<()> {
     let metrics = Metrics::new();
 
-    println!("{:?}", metrics);
+    println!("{:?}", metrics.snapshot());
 
     // 将上面的代码改成多线程处理
     for _ in 0..N {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone()); //clone原理 Metrics {data: Arc::clone(&metrics.data)}
     }
     for i in 0..M {
         task_worker(i, metrics.clone());
@@ -23,24 +23,23 @@ fn main() -> Result<()> {
     loop {
         thread::sleep(Duration::from_secs(3));
         //print! 宏不会自动换行，这里一定要换行,否则不会立即打印,直到一行溢出
-        println!("{:?}", metrics);
+        println!("{:?}", metrics.snapshot());
     }
 }
 
-fn task_worker(idx: usize, mut metrics: Metrics) {
+fn task_worker(idx: usize, metrics: Metrics) {
     thread::spawn(move || loop {
-        thread::sleep(Duration::from_millis(
-            rand::thread_rng().gen_range(100..5000),
-        ));
-        metrics.inc(format!("call.thread.worker.{}", idx));
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
     });
 }
 
-fn request_worker(mut metrics: Metrics) {
+fn request_worker(metrics: Metrics) {
     thread::spawn(move || loop {
         let rng = &mut rand::thread_rng();
         thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(0..256);
-        metrics.inc(format!("request.page.{}", page));
+        let page = rng.gen_range(0..5);
+        metrics.inc(format!("request.page.{}", page)).unwrap();
     });
 }
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,11 +1,16 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use anyhow::{anyhow, Result};
 
 // metrics 的 data structure
 #[derive(Debug, Clone)]
 pub struct Metrics {
     //key 和 value 的类型都可以自定义\
     //这里如果类型写死的话, 就不需要再结构体签名上加泛型<>符号了
-    data: HashMap<String, i64>,
+    data: Arc<Mutex<HashMap<String, i64>>>,
 }
 
 impl Default for Metrics {
@@ -18,21 +23,28 @@ impl Default for Metrics {
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: HashMap::new(),
+            data: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    pub fn inc(&mut self, key: impl Into<String>) {
-        let count = self.data.entry(key.into()).or_insert(0);
-        *count += 1
+    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
+        let mut mutex_guard = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let count = mutex_guard.entry(key.into()).or_insert(0);
+        *count += 1;
+        Ok(())
     }
 
-    pub fn dec(&mut self, key: impl Into<String>) {
-        let count = self.data.entry(key.into()).or_insert(0);
-        *count -= 1
-    }
+    // pub fn dec(&mut self, key: impl Into<String>) {
+    //     let mut arc = self.data.lock().map_err(|e| anyhow!(e.to_string())).unwrap();
+    //     let count = arc.entry(key.into()).or_insert(0);
+    //     *count -= 1
+    // }
 
-    pub fn snapshot(&self) -> HashMap<String, i64> {
-        self.data.clone()
+    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
+        Ok(self
+            .data
+            .lock()
+            .map_err(|e| anyhow!(e.to_string()))?
+            .clone())
     }
 }
